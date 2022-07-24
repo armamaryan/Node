1085d0ca02e96ced419d1591ff3c81210ffd206c
aamaryan <arman_am@instigatemobile.com>
2022-07-24 14:59:01 +0400

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -5,15 +5,14 @@ const Author = require('../models/author')
 // All authors route
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if(req.query.name !== null && req.query.name !== '') {
+    if(req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
         const authors = await Author.find(searchOptions)
         res.render('authors/index', { 
             authors: authors, 
-            searchOptions: req.query
-        })
+            searchOptions: req.query})
     } catch {
         res.redirect('/')
     }
